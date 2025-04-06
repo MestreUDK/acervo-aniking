917fbf8ced2aa653992498b04e2a5fe75accd11a
MestreUDK <117117483+MestreUDK@users.noreply.github.com>
2025-04-05 21:03:49 -0300

    Criar app/page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: 20 }}>
+      <h1>Acervo AniKing</h1>
+      <p>Seu acervo vai aparecer aqui futuramente.</p>
+    </main>
+  );
+}
